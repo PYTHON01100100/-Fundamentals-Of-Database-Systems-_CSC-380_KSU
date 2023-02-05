dd840c4042904e8aee3a55ecffa608d627c2a523
ABDULRAHMAN AL-MYMAN_عبدالرحمن الميمان <66333040+Link20222@users.noreply.github.com>
2023-02-05 09:05:33 +0300

    Add files via upload

---
+++ project 380/testnetbeans/hello/src/main/java/Hello.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Dell
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("5×5="+5*5);
+    }}
